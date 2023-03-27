1b511405b27e6672025d13943486d64881ea48be
Berk <berkpeker94@gmail.com>
2023-03-27 08:58:29 +0300

    added tailwind(nativewind) and fixed assest locations

--- App.tsx
+++ App.tsx
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
+
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+// screens
+import { Home } from "./src/screens/Home";
+import { Settings } from "./src/screens/Settings";
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={Home} />
+        <Tab.Screen name="Settings" component={Settings} />
+      </Tab.Navigator>
+    </NavigationContainer>
+    // <View style={styles.container}>
+    //   <Text>Open up App.tsx to start working on your app!</Text>
+    // <StatusBar style="auto" />
+    // </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/screens/Home.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+import * as React from "react";
+
+export function Home() {
+  return (
+    <View className="flex-1 items-center justify-center bg-sky-600">
+      <Text>Home!</Text>
+    </View>
+  );
+}
