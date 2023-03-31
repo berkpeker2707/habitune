5b41a630d7f5dedd49ddb7878add8f28d1727aab
Berk <berkpeker94@gmail.com>
2023-03-31 15:05:20 +0300

    changed directory of images

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import { StatusBar } from "expo-status-bar";
+import * as React from "react";
 import { Text, View } from "react-native";
 
-import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
