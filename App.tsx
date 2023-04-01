e35a15c0f0fba8dbe0141b2839b172b7d1773e6a
Berk <berkpeker94@gmail.com>
2023-04-01 19:05:18 +0300

    added custom navbar initial

--- App.tsx
+++ App.tsx
@@ -3,7 +3,10 @@ import * as React from "react";
 import { Text, View } from "react-native";
 
 import { NavigationContainer } from "@react-navigation/native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import {
+  createBottomTabNavigator,
+  BottomTabNavigationOptions,
+} from "@react-navigation/bottom-tabs";
 
 // screens
 import { Home } from "./src/screens/Home";
@@ -11,10 +14,24 @@ import { Settings } from "./src/screens/Settings";
 
 const Tab = createBottomTabNavigator();
 
-export default function App() {
+const options: BottomTabNavigationOptions = {
+  headerShown: false,
+  tabBarStyle: {
+    position: "absolute",
+    bottom: 25,
+    left: 20,
+    right: 20,
+    elevation: 0,
+    backgroundColor: "red",
+    borderRadius: 15,
+    height: 90,
+  },
+};
+
+const App = () => {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator screenOptions={options}>
         <Tab.Screen name="Home" component={Home} />
         <Tab.Screen name="Settings" component={Settings} />
       </Tab.Navigator>
@@ -24,4 +41,6 @@ export default function App() {
     // <StatusBar style="auto" />
     // </View>
   );
-}
+};
+
+export default App;
