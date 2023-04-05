f51e325e621e614a00ab547ad9767f5905fd28a3
Berk <berkpeker94@gmail.com>
2023-04-05 13:29:08 +0300

    changed name of settings to stats

--- src/screens/Stats.tsx
+++ src/screens/Stats.tsx
@@ -1,10 +1,10 @@
 import * as React from "react";
 import { Text, View } from "react-native";
 
-export function Settings() {
+export function Stats() {
   return (
     <View className="flex-1 items-center justify-center bg-sky-100">
-      <Text>Settings!</Text>
+      <Text>Stats!</Text>
     </View>
   );
 }
