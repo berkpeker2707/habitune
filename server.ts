c196993a082208f1ce1ad8720241a9652b8b96af
Berk <berkpeker94@gmail.com>
2023-04-05 16:15:41 +0300

    added morgan, winston, concurrently, nodemon libs

--- server.ts
+++ server.ts
@@ -1,15 +1,32 @@
-import express, { Express, Request, Response } from 'express';
-import dotenv from 'dotenv';
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+
+//logger is winston, can log all and categorize all as you wish
+//morgan is for checking requests
+import Logger from "./middlewares/logger";
+import morganMiddleware from "./middlewares/morganMiddleware";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
+app.get("/", (req: Request, res: Response) => {
+  res.send("Express + TypeScript Server");
 });
 
 app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+  console.log(`⚡️[server]: Server is running at port: ${port}`);
+});
+
+//test requests for winston and morgan
+app.use(morganMiddleware);
+app.get("/logger", (_, res) => {
+  Logger.error("This is an error log");
+  Logger.warn("This is a warn log");
+  Logger.info("This is a info log");
+  Logger.http("This is a http log");
+  Logger.debug("This is a debug log");
+
+  res.send("Hello world");
 });
