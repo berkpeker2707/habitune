d665de503ce771467aa2cdb212355375a59421f5
Berk <berkpeker94@gmail.com>
2023-04-27 16:40:26 +0300

    added delete habbit

--- habit/habit.controllers.ts
+++ habit/habit.controllers.ts
@@ -36,3 +36,24 @@ export const createHabit = async (req: IReq | any, res: Response) => {
     return res.status(500).send(getErrorMessage(error));
   }
 };
+
+export const deleteHabit = async (req: IReq | any, res: Response) => {
+  try {
+    await Habit.findOneAndDelete({
+      _id: req.body._id,
+    });
+
+    await User.findOneAndUpdate(
+      { _id: req.user[0]._id },
+      {
+        $pull: { habits: req.body._id },
+      },
+      { upsert: true }
+    );
+
+    res.status(200).json("newHabit");
+  } catch (error) {
+    Logger.error(error);
+    return res.status(500).send(getErrorMessage(error));
+  }
+};
--- habit/habit.routes.ts
+++ habit/habit.routes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { createHabit } from "./habit.controllers";
+import { createHabit, deleteHabit } from "./habit.controllers";
 
 import verifyToken from "../middlewares/verifyToken";
 
 const habitRoutes = Router();
 
 habitRoutes.get("/new", verifyToken, createHabit);
+
+habitRoutes.delete("/delete", verifyToken, deleteHabit);
 
 export default habitRoutes;
