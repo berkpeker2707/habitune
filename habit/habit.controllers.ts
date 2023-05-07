cea4ce237426218d1b261156e3a9661317fb8f47
Berk <berkpeker94@gmail.com>
2023-05-07 18:29:36 +0300

    completed updateHabitColor, updateHabitSharedWith, updateHabitFirstAndLastDate, updateHabitDates

--- habit/habit.controllers.ts
+++ habit/habit.controllers.ts
@@ -58,13 +58,96 @@ export const deleteHabit = async (req: IReq | any, res: Response) => {
   }
 };
 
-export const editHabit = async (req: IReq | any, res: Response) => {
+export const updateHabitColor = async (req: IReq | any, res: Response) => {
   try {
-    const selectedHabit = await Habit.find({
-      _id: req.body._id,
-    });
+    const selectedHabit = await Habit.findByIdAndUpdate(
+      req.body._id,
+      {
+        $set: { color: req.body.color },
+      },
+      { new: true }
+    );
 
     res.status(200).json(selectedHabit);
+  } catch (error) {
+    Logger.error(error);
+    return res.status(500).send(getErrorMessage(error));
+  }
+};
+
+export const updateHabitSharedWith = async (req: IReq | any, res: Response) => {
+  try {
+    const selectedHabit = await Habit.findById(req.body._id);
+    const alreadySharedWith = selectedHabit?.sharedWith.some((elemfriends) => {
+      return elemfriends.toString() === req.body.userId.toString();
+    });
+    if (alreadySharedWith) {
+      const updatedSelectedHabit = await Habit.findByIdAndUpdate(
+        req.body._id,
+        { $pull: { sharedWith: req.body.userId } },
+        { new: true }
+      );
+      res.status(200).json(updatedSelectedHabit);
+    } else {
+      const updatedSelectedHabit = await Habit.findByIdAndUpdate(
+        req.body._id,
+        { $push: { sharedWith: req.body.userId } },
+        { new: true }
+      );
+      res.status(200).json(updatedSelectedHabit);
+    }
+  } catch (error) {
+    Logger.error(error);
+    return res.status(500).send(getErrorMessage(error));
+  }
+};
+
+export const updateHabitFirstAndLastDate = async (
+  req: IReq | any,
+  res: Response
+) => {
+  try {
+    if (req.body.lastDate > req.body.firstDate) {
+      const selectedHabit = await Habit.findByIdAndUpdate(
+        req.body._id,
+        {
+          $set: { firstDate: req.body.firstDate, lastDate: req.body.lastDate },
+        },
+        { upsert: false, new: true }
+      );
+      res.status(200).json(selectedHabit);
+    } else {
+      res.status(500).json("Last date cannot be earlier than first date.");
+    }
+  } catch (error) {
+    Logger.error(error);
+    return res.status(500).send(getErrorMessage(error));
+  }
+};
+
+export const updateHabitDates = async (req: IReq | any, res: Response) => {
+  try {
+    const selectedHabit = await Habit.findById(req.body._id);
+    const dateExists = selectedHabit?.dates.some((elemfriends) => {
+      return elemfriends.getTime().toString() === req.body.date.toString();
+    });
+    if (dateExists) {
+      const updatedSelectedHabit = await Habit.findByIdAndUpdate(
+        req.body._id,
+        { $pull: { dates: req.body.date } },
+        { new: true }
+      );
+      console.log(true);
+      res.status(200).json(updatedSelectedHabit);
+    } else {
+      const updatedSelectedHabit = await Habit.findByIdAndUpdate(
+        req.body._id,
+        { $push: { dates: req.body.date } },
+        { new: true }
+      );
+      console.log(false);
+      res.status(200).json(updatedSelectedHabit);
+    }
   } catch (error) {
     Logger.error(error);
     return res.status(500).send(getErrorMessage(error));
--- habit/habit.routes.ts
+++ habit/habit.routes.ts
@@ -1,5 +1,12 @@
 import { Router } from "express";
-import { createHabit, deleteHabit, editHabit } from "./habit.controllers";
+import {
+  createHabit,
+  deleteHabit,
+  updateHabitColor,
+  updateHabitSharedWith,
+  updateHabitFirstAndLastDate,
+  updateHabitDates,
+} from "./habit.controllers";
 
 import verifyToken from "../middlewares/verifyToken";
 
@@ -9,6 +16,16 @@ habitRoutes.get("/new", verifyToken, createHabit);
 
 habitRoutes.delete("/delete", verifyToken, deleteHabit);
 
-habitRoutes.put("/edit", verifyToken, editHabit);
+habitRoutes.put("/update/color", verifyToken, updateHabitColor);
+
+habitRoutes.put("/update/share", verifyToken, updateHabitSharedWith);
+
+habitRoutes.put(
+  "/update/firstAndLastDate",
+  verifyToken,
+  updateHabitFirstAndLastDate
+);
+
+habitRoutes.put("/update/date", verifyToken, updateHabitDates);
 
 export default habitRoutes;
