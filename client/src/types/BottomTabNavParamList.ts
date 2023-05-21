d9961f4bcf100762fdca72f93448a67d58621e51
Berk <berkpeker94@gmail.com>
2023-05-21 13:32:11 +0300

    fixed BottomTabNavParamList and added HomeNavParam (Stack)

---
+++ client/src/types/BottomTabNavParamList.ts
@@ -0,0 +1,20 @@
+import type { BottomTabNavigationProp } from "@react-navigation/bottom-tabs";
+import type { StackNavigationProp } from "@react-navigation/stack";
+import { CompositeNavigationProp } from "@react-navigation/core";
+
+export type BottomTabNavParamList = {
+  Signin: undefined;
+  HomeSection: undefined;
+  Add: undefined;
+  Overview: undefined;
+};
+
+export type HomeNavParamList = {
+  Icon: undefined;
+  Profile: undefined;
+};
+
+export type generalScreenProp = CompositeNavigationProp<
+  BottomTabNavigationProp<BottomTabNavParamList>,
+  StackNavigationProp<HomeNavParamList>
+>;
