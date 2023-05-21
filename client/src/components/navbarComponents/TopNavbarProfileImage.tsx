785c882e5204eb09b5ad6e981572a72f9f4caa8d
Berk <berkpeker94@gmail.com>
2023-05-21 13:30:45 +0300

    added TopNavbarProfileImage

---
+++ client/src/components/navbarComponents/TopNavbarProfileImage.tsx
@@ -0,0 +1,38 @@
+import * as React from "react";
+import { Image, Platform, View } from "react-native";
+
+Image;
+const TopNavbarProfileImage = (props: any) => {
+  const { imageSource } = props;
+
+  return (
+    <View
+      style={
+        Platform.OS === "android"
+          ? {
+              elevation: 6,
+              shadowColor: "black",
+            }
+          : {
+              shadowColor: "black",
+              shadowOffset: { width: 1, height: 2 },
+              shadowOpacity: 0.3,
+            }
+      }
+    >
+      <Image
+        source={{ uri: imageSource }}
+        style={{
+          width: 35,
+          height: 35,
+          borderRadius: 150 / 2,
+          overflow: "hidden",
+          // borderWidth: 0.5,
+          // borderColor: "red",
+        }}
+      />
+    </View>
+  );
+};
+
+export default TopNavbarProfileImage;
