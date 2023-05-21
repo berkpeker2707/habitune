9e9101aea41cf62f8f7de82df719750d7face91b
Berk <berkpeker94@gmail.com>
2023-05-21 13:30:16 +0300

    added settings and profile screens

---
+++ client/src/screens/Settings.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import { Text, View } from "react-native";
+
+export function Settings() {
+  return (
+    <View className="flex-1 items-center justify-center bg-sky-100">
+      <Text>Settings!</Text>
+    </View>
+  );
+}
