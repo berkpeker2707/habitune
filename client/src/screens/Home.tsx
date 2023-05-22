d7f17e4a3b104369cc6ebe8bfb98b3faf62a9baa
Berk <berkpeker94@gmail.com>
2023-05-22 23:13:31 +0300

    Added HabitBar and HabitBarFilled to home for starters

--- client/src/screens/Home.tsx
+++ client/src/screens/Home.tsx
@@ -1,10 +1,100 @@
 import * as React from "react";
 import { Text, View } from "react-native";
+import { FlashList } from "@shopify/flash-list";
+import HabitBar from "../components/home/HabitBar";
+import HabitBarFilled from "../components/home/HabitBarFilled";
+
+const DATA = [
+  {
+    title: "First Item",
+  },
+  {
+    title: "Second Item",
+  },
+  {
+    title: "Second Item",
+  },
+  {
+    title: "Second Item",
+  },
+  {
+    title: "Second Item",
+  },
+  {
+    title: "Second Item",
+  },
+  {
+    title: "Second Item",
+  },
+  {
+    title: "Second Item",
+  },
+  {
+    title: "Second Item",
+  },
+  {
+    title: "Second Item",
+  },
+  {
+    title: "Second Item",
+  },
+  {
+    title: "Second Item",
+  },
+  {
+    title: "Second Item",
+  },
+  {
+    title: "Second Item",
+  },
+  {
+    title: "Second Item",
+  },
+  {
+    title: "Second Item",
+  },
+  {
+    title: "Second Item",
+  },
+  {
+    title: "Second Item",
+  },
+  {
+    title: "Second Item",
+  },
+  {
+    title: "Second Item",
+  },
+];
+
+const renderItem = ({ ...DATA }) => (
+  // <Text
+  //   style={{
+  //     flex: 1,
+  //     flexDirection: "column",
+  //     height: "100%",
+  //     alignItems: "center",
+  //     justifyContent: "center",
+  //     textAlign: "center",
+  //   }}
+  // >
+  //   {DATA.item.title}
+  // </Text>
+  // <HabitBar />
+  <HabitBarFilled />
+);
 
 export function Home() {
   return (
-    <View className="flex-1 items-center justify-center bg-sky-100">
-      <Text>Home!</Text>
+    <View
+      style={{
+        display: "flex",
+        height: "100%",
+        backgroundColor: "#FFFFFF",
+      }}
+    >
+      <Text>Habits</Text>
+      <FlashList data={DATA} renderItem={renderItem} estimatedItemSize={20} />
     </View>
   );
 }
