836f4588307bbb87189da8b5ffa6ce30fd3dbb13
Berk <berkpeker94@gmail.com>
2023-06-01 14:46:08 +0300

    habitBarFilled fixed a bit with ternaries

--- client/src/components/home/HabitBarFilled.tsx
+++ client/src/components/home/HabitBarFilled.tsx
@@ -36,69 +36,95 @@ const HabitBarFilled = (props: any) => {
       <Text fill="#000" fontSize="19" x={40} y={30}>
         {item.name}
       </Text>
-      <G filter="url(#filter2_d_386_5008)" shapeRendering="crispEdges">
-        <Rect
-          width={34}
-          height={34}
-          x={286}
-          y={6}
-          fill="url(#pattern0)"
-          rx={17}
-        />
-        <Rect
-          width={34.5}
-          height={34.5}
-          x={285.75}
-          y={5.75}
-          stroke="#fff"
-          strokeWidth={1}
-          rx={17.25}
-        />
-      </G>
-      {/* <G filter="url(#filter3_d_386_5008)" shapeRendering="crispEdges">
-        <Rect
-          width={33}
-          height={34}
-          x={306}
-          y={6}
-          fill="url(#pattern1)"
-          rx={16.5}
-        />
-        <Rect
-          width={33.5}
-          height={34.5}
-          x={305.75}
-          y={5.75}
-          stroke="#fff"
-          strokeWidth={1}
-          rx={16.75}
-        />
-      </G> */}
-      <G filter="url(#filter4_d_386_5008)">
-        <Rect width={34} height={34} x={325} y={6} fill="#9890B2" rx={17} />
-        <Rect
-          width={34.5}
-          height={34.5}
-          x={324.75}
-          y={5.75}
-          stroke="#fff"
-          strokeWidth={1}
-          rx={17.25}
-        />
-      </G>
-      <Text fill="#fff" fontSize="14" fontWeight={600} x={336} y={29}>
-        +1
-      </Text>
+      {item &&
+      item.sharedWith[item.sharedWith.length - 2] &&
+      item.sharedWith[item.sharedWith.length - 2].image ? (
+        <G filter="url(#filter2_d_386_5008)" shapeRendering="crispEdges">
+          <Rect
+            width={34}
+            height={34}
+            x={286}
+            y={6}
+            fill="url(#pattern0)"
+            rx={17}
+          />
+          <Rect
+            width={34.5}
+            height={34.5}
+            x={285.75}
+            y={5.75}
+            stroke="#fff"
+            strokeWidth={1}
+            rx={17.25}
+          />
+        </G>
+      ) : (
+        <></>
+      )}
+      {item &&
+      item.sharedWith[item.sharedWith.length - 1] &&
+      item.sharedWith[item.sharedWith.length - 1].image ? (
+        <G filter="url(#filter3_d_386_5008)" shapeRendering="crispEdges">
+          <Rect
+            width={33}
+            height={34}
+            x={306}
+            y={6}
+            fill="url(#pattern1)"
+            rx={16.5}
+          />
+          <Rect
+            width={33.5}
+            height={34.5}
+            x={305.75}
+            y={5.75}
+            stroke="#fff"
+            strokeWidth={1}
+            rx={16.75}
+          />
+        </G>
+      ) : (
+        <></>
+      )}
+      {item && item.sharedWith && item.sharedWith.length > 1 ? (
+        <>
+          <G filter="url(#filter4_d_386_5008)">
+            <Rect width={34} height={34} x={325} y={6} fill="#9890B2" rx={17} />
+            <Rect
+              width={34.5}
+              height={34.5}
+              x={324.75}
+              y={5.75}
+              stroke="#fff"
+              strokeWidth={1}
+              rx={17.25}
+            />
+          </G>
+          <Text fill="#fff" fontSize="14" fontWeight={600} x={336} y={29}>
+            +{item.sharedWith.length}
+          </Text>
+        </>
+      ) : (
+        <></>
+      )}
       <Defs>
-        <Pattern
-          id="pattern0"
-          width={1}
-          height={1}
-          patternContentUnits="objectBoundingBox"
-        >
-          <Use transform="scale(.0025)" xlinkHref="#image0_386_5008" />
-        </Pattern>
-        {item.sharedWith[1]?.image ? (
+        {item &&
+        item.sharedWith[item.sharedWith.length - 2] &&
+        item.sharedWith[item.sharedWith.length - 2].image ? (
+          <Pattern
+            id="pattern0"
+            width={1}
+            height={1}
+            patternContentUnits="objectBoundingBox"
+          >
+            <Use transform="scale(.0025)" xlinkHref="#image0_386_5008" />
+          </Pattern>
+        ) : (
+          <></>
+        )}
+        {item &&
+        item.sharedWith[item.sharedWith.length - 1] &&
+        item.sharedWith[item.sharedWith.length - 1].image ? (
           <Pattern
             id="pattern1"
             width={1}
@@ -124,14 +150,21 @@ const HabitBarFilled = (props: any) => {
           <Stop stopColor="#fff" />
           <Stop offset={1} stopColor={item.color} />
         </LinearGradient>
-        <Image
-          id="image0_386_5008"
-          width={399}
-          height={399}
-          href={{ uri: item.sharedWith[0].image }}
-          // href={{ uri: "https://i.pravatar.cc/300" }}
-        />
-        {item.sharedWith[1]?.image ? (
+        {item &&
+        item.sharedWith[item.sharedWith.length - 2] &&
+        item.sharedWith[item.sharedWith.length - 2].image ? (
+          <Image
+            id="image0_386_5008"
+            width={399}
+            height={399}
+            href={{ uri: item.sharedWith[item.sharedWith.length - 2].image }}
+          />
+        ) : (
+          <></>
+        )}
+        {item &&
+        item.sharedWith[item.sharedWith.length - 1] &&
+        item.sharedWith[item.sharedWith.length - 1].image ? (
           <Image
             id="image1_386_5008"
             width={1080}
