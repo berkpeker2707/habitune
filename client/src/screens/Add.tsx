17bc91ffc77cc5bdfdffb29287c10b0944d08de5
Berk <berkpeker94@gmail.com>
2023-06-02 10:03:47 +0300

    added AddTaskGroup as svg to Add

--- client/src/screens/Add.tsx
+++ client/src/screens/Add.tsx
@@ -1,10 +1,12 @@
 import * as React from "react";
 import { Text, View } from "react-native";
+import AddTaskGroup from "../components/add/AddTaskGroup";
 
 export function Add() {
   return (
     <View className="flex-1 items-center justify-center bg-sky-100">
       <Text>Add</Text>
+      <AddTaskGroup />
     </View>
   );
 }
