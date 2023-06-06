cd10da39591203a08e6410a3bc7048ebdc9d20b9
Berk <berkpeker94@gmail.com>
2023-06-06 11:56:54 +0300

    removed single parts from add which are added just for svg testing

--- client/src/screens/Add.tsx
+++ client/src/screens/Add.tsx
@@ -4,6 +4,11 @@ import AddTaskGroup from "../components/add/AddTaskGroup";
 import AddTaskFrequency from "../components/add/AddTaskFrequency";
 import AddTaskShare from "../components/add/AddTaskShare";
 import AddTaskShareONLY from "../components/add/AddTaskShareONLY";
+import TaskName from "../components/add/TaskName";
+import Frequency from "../components/add/Frequency";
+import ShareWithYourFriends from "../components/add/ShareWithYourFriends";
+import Reminder from "../components/add/Reminder";
+import Color from "../components/add/Color";
 
 export function Add() {
   return (
@@ -18,10 +23,13 @@ export function Add() {
     >
       <ScrollView>
         <Text>Add</Text>
-        {/* <AddTaskGroup /> */}
-        {/* <AddTaskFrequency /> */}
-        <AddTaskShare />
-        {/* <AddTaskShareONLY /> */}
+        <TaskName />
+        <Frequency />
+        <ShareWithYourFriends />
+        <Reminder />
+        <Color />
+
+        {/* <AddTaskShare /> */}
       </ScrollView>
     </View>
   );
