ca25182b34fa88a1a123598589ff9786396c952a
Berk <berkpeker94@gmail.com>
2023-06-17 16:09:41 +0300

    Removed old svg from TaskName

--- client/src/components/add/TaskName.tsx
+++ client/src/components/add/TaskName.tsx
@@ -10,20 +10,7 @@ const TaskName = (props: any) => {
     props.sendNewHabitNameState(taskName);
   }, [taskName]);
   return (
-    <View>
-      {/* <Svg width={345} height={48} viewBox="0 0 345 48" fill="none" {...props}>
-        <Rect
-          x={0.25}
-          y={0.25}
-          width={344.5}
-          height={39.5}
-          rx={19.75}
-          stroke="#968EB0"
-          strokeWidth={0.5}
-        />
-        <Text x={20} y={25} fontSize="14" fontWeight={400} fill="#444">
-          Task Name
-        </Text> */}
+    <View style={{ width: 345 }}>
       <TextInput
         style={{
           height: 39.5,
@@ -36,7 +23,6 @@ const TaskName = (props: any) => {
         onChangeText={(text) => setTaskName(text)}
         maxLength={30}
       />
-      {/* </Svg> */}
     </View>
   );
 };
