ee4a81cb46dad67f4a61062d3fb7a274e513dd04
Berk <berkpeker94@gmail.com>
2023-06-17 16:13:35 +0300

    commented old svg of AddTaskFrequency

--- client/src/screens/Add.tsx
+++ client/src/screens/Add.tsx
@@ -45,6 +45,7 @@ export function Add(props: any) {
         <Text>Add</Text>
         <TaskName sendNewHabitNameState={sendNewHabitNameState} />
         <Frequency />
+        {/* <AddTaskFrequency /> */}
         <ShareWithYourFriends />
         <Reminder />
         <Color />
