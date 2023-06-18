b44e4d4afa033a776b6166741af55d1fd9982482
Berk <berkpeker94@gmail.com>
2023-06-18 15:01:43 +0300

    removed svg and added Color component

--- client/src/components/add/Color.tsx
+++ client/src/components/add/Color.tsx
@@ -1,34 +1,165 @@
 import * as React from "react";
-import Svg, { Rect, Text, Circle } from "react-native-svg";
+import { TextInput, TouchableOpacity, View } from "react-native";
 
 function Color(props: any) {
   return (
-    <Svg width={345} height={68} viewBox="0 0 345 68" fill="none" {...props}>
-      <Rect
-        x={0.25}
-        y={20.25}
-        width={344.5}
-        height={39.5}
-        rx={19.75}
-        stroke="#968EB0"
-        strokeWidth={0.5}
-      />
-      <Text x={20} y={13} fontSize="14" fontWeight={400} fill="#444">
+    <>
+      <TextInput
+        style={{
+          height: 29.5,
+          paddingLeft: 20,
+
+          color: "#444",
+        }}
+        editable={false}
+        selectTextOnFocus={false}
+      >
         Color
-      </Text>
-      <Circle cx={47} cy={40} r={10} fill="#9DB2CE" />
-      <Circle cx={103} cy={40} r={10} fill="#A5D2AC" />
-      <Circle cx={75} cy={40} r={10} fill="#C04F43" />
-      <Circle cx={19} cy={40} r={10} fill="#968EB0" />
-      <Circle cx={159} cy={40} r={10} fill="#F59732" />
-      <Circle cx={131} cy={40} r={10} fill="#99BB42" />
-      <Circle cx={187} cy={40} r={10} fill="#F1867E" />
-      <Circle cx={215} cy={40} r={10} fill="#FCCA1B" />
-      <Circle cx={271} cy={40} r={10} fill="#6EA8D8" />
-      <Circle cx={243} cy={40} r={10} fill="#4D6691" />
-      <Circle cx={299} cy={40} r={10} fill="#DEB4CF" />
-      <Circle cx={327} cy={40} r={10} fill="#F6AF90" />
-    </Svg>
+      </TextInput>
+      <View
+        style={{
+          width: 345,
+          height: 39.5,
+          borderWidth: 0.5,
+          borderRadius: 20,
+          paddingLeft: 20,
+          marginBottom: 10,
+        }}
+      >
+        <TouchableOpacity
+          style={{
+            position: "absolute",
+            height: 20,
+            width: 20,
+            left: 9,
+            top: 9,
+            borderRadius: 15,
+            backgroundColor: "#9DB2CE",
+          }}
+        ></TouchableOpacity>
+        <TouchableOpacity
+          style={{
+            position: "absolute",
+            height: 20,
+            width: 20,
+            left: 37,
+            top: 9,
+            borderRadius: 15,
+            backgroundColor: "#A5D2AC",
+          }}
+        ></TouchableOpacity>
+        <TouchableOpacity
+          style={{
+            position: "absolute",
+            height: 20,
+            width: 20,
+            left: 65,
+            top: 9,
+            borderRadius: 15,
+            backgroundColor: "#C04F43",
+          }}
+        ></TouchableOpacity>
+        <TouchableOpacity
+          style={{
+            position: "absolute",
+            height: 20,
+            width: 20,
+            left: 93,
+            top: 9,
+            borderRadius: 15,
+            backgroundColor: "#968EB0",
+          }}
+        ></TouchableOpacity>
+        <TouchableOpacity
+          style={{
+            position: "absolute",
+            height: 20,
+            width: 20,
+            left: 121,
+            top: 9,
+            borderRadius: 15,
+            backgroundColor: "#F59732",
+          }}
+        ></TouchableOpacity>
+        <TouchableOpacity
+          style={{
+            position: "absolute",
+            height: 20,
+            width: 20,
+            left: 149,
+            top: 9,
+            borderRadius: 15,
+            backgroundColor: "#99BB42",
+          }}
+        ></TouchableOpacity>
+        <TouchableOpacity
+          style={{
+            position: "absolute",
+            height: 20,
+            width: 20,
+            left: 177,
+            top: 9,
+            borderRadius: 15,
+            backgroundColor: "#F1867E",
+          }}
+        ></TouchableOpacity>
+        <TouchableOpacity
+          style={{
+            position: "absolute",
+            height: 20,
+            width: 20,
+            left: 205,
+            top: 9,
+            borderRadius: 15,
+            backgroundColor: "#FCCA1B",
+          }}
+        ></TouchableOpacity>
+        <TouchableOpacity
+          style={{
+            position: "absolute",
+            height: 20,
+            width: 20,
+            left: 233,
+            top: 9,
+            borderRadius: 15,
+            backgroundColor: "#6EA8D8",
+          }}
+        ></TouchableOpacity>
+        <TouchableOpacity
+          style={{
+            position: "absolute",
+            height: 20,
+            width: 20,
+            left: 261,
+            top: 9,
+            borderRadius: 15,
+            backgroundColor: "#4D6691",
+          }}
+        ></TouchableOpacity>
+        <TouchableOpacity
+          style={{
+            position: "absolute",
+            height: 20,
+            width: 20,
+            left: 289,
+            top: 9,
+            borderRadius: 15,
+            backgroundColor: "#DEB4CF",
+          }}
+        ></TouchableOpacity>
+        <TouchableOpacity
+          style={{
+            position: "absolute",
+            height: 20,
+            width: 20,
+            left: 317,
+            top: 9,
+            borderRadius: 15,
+            backgroundColor: "#F6AF90",
+          }}
+        ></TouchableOpacity>
+      </View>
+    </>
   );
 }
 
