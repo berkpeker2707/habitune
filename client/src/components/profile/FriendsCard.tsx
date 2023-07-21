775f3ab24ca0d80766f5a80f94040b57472d7616
Berk <berkpeker94@gmail.com>
2023-07-21 18:59:34 +0300

    changed FriendsList with FriendsCard(not be able to interact)

--- client/src/components/profile/FriendsCard.tsx
+++ client/src/components/profile/FriendsCard.tsx
@@ -1,92 +1,54 @@
 import * as React from "react";
-import {
-  ScrollView,
-  Text,
-  TextInput,
-  TouchableOpacity,
-  View,
-} from "react-native";
+import { View, ScrollView } from "react-native";
 import FriendBar from "../add/shareComponents/FriendBar";
 
-const FriendsCard = () => {
+const FriendsCard = (props: any) => {
   return (
-    <ScrollView
-      style={{
-        // marginBottom: 85,
-        // width: "100%",
-        height: 700,
-      }}
-    >
+    <ScrollView>
       <View
         style={{
-          // display: "flex",
-          // height: 49 * (1 + 1),
-          width: 324,
-          right: 21,
-          backgroundColor: "#FFF44F",
-          // justifyContent: "center",
-          // alignItems: "center",
-
           flex: 1,
           height: 49 * (5 + 1),
         }}
       >
-        <TouchableOpacity onPress={() => console.log("1")}>
+        <View>
           <FriendBar
             friendProfilePicture={"https://i.pravatar.cc/300"}
-            friendName={"Tuğçe"}
-            friendSelected={false}
+            friendName={"İrem"}
             barPositionLevel={49 * 0}
-          />
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => console.log("2")}>
-          <FriendBar
-            friendProfilePicture={"https://i.pravatar.cc/300"}
-            friendName={"Tuğçe"}
             friendSelected={false}
-            barPositionLevel={49 * 1}
           />
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => console.log("3")}>
-          <FriendBar
-            friendProfilePicture={"https://i.pravatar.cc/300"}
-            friendName={"Tuğçe"}
-            friendSelected={false}
-            barPositionLevel={49 * 2}
-          />
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => console.log("4")}>
-          <FriendBar
-            friendProfilePicture={"https://i.pravatar.cc/300"}
-            friendName={"Tuğçe"}
-            friendSelected={false}
-            barPositionLevel={49 * 3}
-          />
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => console.log("5")}>
-          <FriendBar
-            friendProfilePicture={"https://i.pravatar.cc/300"}
-            friendName={"Tuğçe"}
-            friendSelected={false}
-            barPositionLevel={49 * 4}
-          />
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => console.log("6")}>
-          <FriendBar
-            friendProfilePicture={"https://i.pravatar.cc/300"}
-            friendName={"Tuğçe"}
-            friendSelected={false}
-            barPositionLevel={49 * 5}
-          />
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => console.log("7")}>
-          <FriendBar
-            friendProfilePicture={"https://i.pravatar.cc/300"}
-            friendName={"Tuğçe"}
-            friendSelected={false}
-            barPositionLevel={49 * 6}
-          />
-        </TouchableOpacity>
+        </View>
+        {/* <FriendBar
+          friendProfilePicture={"https://i.pravatar.cc/300"}
+          friendName={"Tuğçe"}
+          friendSelected={false}
+          barPositionLevel={49 * 1}
+        />
+        <FriendBar
+          friendProfilePicture={"https://i.pravatar.cc/300"}
+          friendName={"Sümeyye"}
+          friendSelected={true}
+          barPositionLevel={49 * 2}
+        />
+        <FriendBar
+          friendProfilePicture={"https://i.pravatar.cc/300"}
+          friendName={"Doğa"}
+          friendSelected={false}
+          barPositionLevel={49 * 3}
+        />
+        <FriendBar
+          friendProfilePicture={"https://i.pravatar.cc/300"}
+          friendName={"Merve"}
+          friendSelected={false}
+          barPositionLevel={49 * 4}
+        />
+        <FriendBar
+          friendProfilePicture={"https://i.pravatar.cc/300"}
+          friendName={"Özden"}
+          friendSelected={false}
+          barPositionLevel={49 * 5}
+        /> */}
       </View>
     </ScrollView>
   );
--- client/src/screens/Profile.tsx
+++ client/src/screens/Profile.tsx
@@ -1,9 +1,7 @@
 import * as React from "react";
-import { ScrollView, TextInput, View } from "react-native";
+import { ScrollView, View } from "react-native";
 import ProfileCard from "../components/profile/ProfileCard";
 import FriendsCard from "../components/profile/FriendsCard";
-import FriendList from "../components/add/shareComponents/FriendList";
-import ShareWithNumber from "../components/add/shareComponents/ShareWithNumber";
 import AddFriendsButton from "../components/profile/AddFriendsButton";
 
 export function Profile(props: any) {
@@ -23,15 +21,14 @@ export function Profile(props: any) {
         }}
       >
         <ProfileCard />
-        {/* <FriendsCard /> */}
         <View
           style={{
             width: 345,
-            backgroundColor: "yellow",
+            backgroundColor: "#FFFFFF",
           }}
         >
           <AddFriendsButton />
-          <FriendList navigation={props.navigation} />
+          <FriendsCard />
         </View>
       </ScrollView>
     </View>
