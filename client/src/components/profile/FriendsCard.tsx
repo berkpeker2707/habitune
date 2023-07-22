7057f8abce17dac4be9dcecacb726d4f1936233e
Berk <berkpeker94@gmail.com>
2023-07-22 14:34:55 +0300

    little fix at FriendsCard

--- client/src/components/profile/FriendsCard.tsx
+++ client/src/components/profile/FriendsCard.tsx
@@ -11,44 +11,12 @@ const FriendsCard = (props: any) => {
           height: 49 * (5 + 1),
         }}
       >
-        <View>
-          <FriendBar
-            friendProfilePicture={"https://i.pravatar.cc/300"}
-            friendName={"İrem"}
-            barPositionLevel={49 * 0}
-            friendSelected={false}
-          />
-        </View>
-        {/* <FriendBar
-          friendProfilePicture={"https://i.pravatar.cc/300"}
-          friendName={"Tuğçe"}
-          friendSelected={false}
-          barPositionLevel={49 * 1}
-        />
         <FriendBar
           friendProfilePicture={"https://i.pravatar.cc/300"}
-          friendName={"Sümeyye"}
-          friendSelected={true}
-          barPositionLevel={49 * 2}
-        />
-        <FriendBar
-          friendProfilePicture={"https://i.pravatar.cc/300"}
-          friendName={"Doğa"}
+          friendName={"İrem"}
+          barPositionLevel={49 * 0}
           friendSelected={false}
-          barPositionLevel={49 * 3}
         />
-        <FriendBar
-          friendProfilePicture={"https://i.pravatar.cc/300"}
-          friendName={"Merve"}
-          friendSelected={false}
-          barPositionLevel={49 * 4}
-        />
-        <FriendBar
-          friendProfilePicture={"https://i.pravatar.cc/300"}
-          friendName={"Özden"}
-          friendSelected={false}
-          barPositionLevel={49 * 5}
-        /> */}
       </View>
     </ScrollView>
   );
