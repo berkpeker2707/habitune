c6a94f4ae8c1360e87fba2d717a429cf9e740c4a
Berk <berkpeker94@gmail.com>
2023-08-24 18:21:45 +0300

    added updateHabitCompletedDatePayload type to slice

--- client/src/state/habitSlice.ts
+++ client/src/state/habitSlice.ts
@@ -253,7 +253,7 @@ export const updateHabitDatesAction = createAsyncThunk(
 export const updateHabitCompletedDateAction = createAsyncThunk(
   "habit/updateHabitCompletedDate",
   async (
-    updateHabitCompletedDatePayload,
+    updateHabitCompletedDatePayload: {},
     { rejectWithValue, getState, dispatch }
   ) => {
     //get user token
