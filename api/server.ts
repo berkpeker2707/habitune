1bddea9364d574948394cb7cec3bda36d008b3ec
Berk <berkpeker94@gmail.com>
2023-10-16 21:38:14 +0300

    new regex build with Ö

--- api/server.ts
+++ api/server.ts
@@ -56,7 +56,11 @@ app.use(morganMiddleware);
 //   res.send("Hello world");
 // });
 
-const key = process.env.FIREBASE_ADMINSDK_PRIVATE_KEY?.replace(/\\n/g, "\n");
+// const key = process.env.FIREBASE_ADMINSDK_PRIVATE_KEY?.replace(/\\n/g, "\n");
+const key = process.env.FIREBASE_ADMINSDK_PRIVATE_KEY?.replace(
+  /Ö/g,
+  ""
+).replace(/\\n/g, "\n");
 
 var admin = require("firebase-admin");
 
