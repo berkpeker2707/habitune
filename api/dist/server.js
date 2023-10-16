3ca37ed207b833414dc50b20c2ad835579043830
Berk <berkpeker94@gmail.com>
2023-10-16 16:20:03 +0300

    new firebase json env added to server

--- api/dist/server.js
+++ api/dist/server.js
@@ -46,9 +46,21 @@ app.use(morganMiddleware_1.default);
 //   res.send("Hello world");
 // });
 var admin = require("firebase-admin");
-var serviceAccount = require("./habitune-395006-firebase-adminsdk-yxw8e-3842870d9c.json");
+var habitune_395006_firebase_adminsdk_yxw8e_3842870d9c = {
+    type: process.env.FIREBASE_ADMINSDK_TYPE,
+    project_id: process.env.FIREBASE_ADMINSDK_PROJECT_ID,
+    private_key_id: process.env.FIREBASE_ADMINSDK_PRIVATE_KEY_ID,
+    private_key: process.env.FIREBASE_ADMINSDK_PRIVATE_KEY,
+    client_email: process.env.FIREBASE_ADMINSDK_CLIENT_EMAIL,
+    client_id: process.env.FIREBASE_ADMINSDK_CLIENT_ID,
+    auth_uri: process.env.FIREBASE_ADMINSDK_AUTH_URI,
+    token_uri: process.env.FIREBASE_ADMINSDK_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.FIREBASE_ADMINSDK_AUTH_CERT_URL,
+    client_x509_cert_url: process.env.FIREBASE_ADMINSDK_CLIENT_CERT_URL,
+    universe_domain: process.env.FIREBASE_ADMINSDK_UNIVERSE_DOMAIN,
+};
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.cert(habitune_395006_firebase_adminsdk_yxw8e_3842870d9c),
 });
 app.get("/privacy", function (req, res) {
     res.sendFile(path_1.default.join(__dirname, "/view/privacy.html"));
