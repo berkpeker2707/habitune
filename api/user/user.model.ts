630d4d38cfaa84dc8eae4a11f311c93bcc351d4d
Berk <berkpeker94@gmail.com>
2023-10-18 19:48:00 +0300

    added unique: true, index: true to user and notification model to prevent dublication

--- api/user/user.model.ts
+++ api/user/user.model.ts
@@ -5,7 +5,7 @@ const userSchema = new Schema<IUser>({
   id: { type: Number },
   firstName: { type: String },
   lastName: { type: String },
-  email: { type: String },
+  email: { type: String, unique: true, index: true },
   image: { type: String },
   habits: [mongoose.Schema.Types.ObjectId],
   friends: [{ friend: mongoose.Schema.Types.ObjectId, pending: Boolean }],
