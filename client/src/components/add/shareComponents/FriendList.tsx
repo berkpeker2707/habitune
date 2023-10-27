051e56147ea2d338559fa64b7a1119390219f63e
Berk <berkpeker94@gmail.com>
2023-10-27 16:37:04 +0300

    moved all props in from parent in FriendList

--- client/src/components/add/shareComponents/FriendList.tsx
+++ client/src/components/add/shareComponents/FriendList.tsx
@@ -2,65 +2,61 @@ import * as React from "react";
 import { TouchableOpacity, View } from "react-native";
 import { ScrollView } from "react-native-gesture-handler";
 import FriendBar from "./FriendBar";
-import { useEffect, useState } from "react";
 
 import uuid from "react-native-uuid";
 
-const FriendList = (props: any) => {
-  const { currentUser } = props;
-
-  const [shareWithFriendList, setShareWithFriendList] = useState<String[]>([]);
-
-  //updating params if shareWithFriendList changes starts
-  useEffect(() => {
-    props.navigation.setParams({
-      friendList: shareWithFriendList,
-    });
-  }, [shareWithFriendList]);
-  //updating params if shareWithFriendList changes ends
+const FriendList = (props: {
+  currentUser: { friends: Array<object>; pending: boolean };
+  shareWithFriendList: string[];
+  setShareWithFriendList: Function;
+}) => {
+  const { currentUser, shareWithFriendList, setShareWithFriendList } = props;
 
   return (
     <ScrollView>
       {/* filter and map only pending is false, friendship accepted users */}
-      {currentUser.friends
-        .filter(
-          (friendsItemPending: { pending: boolean }) =>
-            friendsItemPending.pending == false
-        )
-        .map((friendsItem: any) => {
-          return (
-            <View
-              key={uuid.v4() as string}
-              style={{
-                flex: 1,
-                height: 49,
-              }}
-            >
-              <TouchableOpacity
-                onPress={() =>
-                  shareWithFriendList.includes(friendsItem.friend._id)
-                    ? setShareWithFriendList(() =>
-                        shareWithFriendList.filter(
-                          (item) => item !== friendsItem.friend._id
-                        )
-                      )
-                    : setShareWithFriendList(() => [friendsItem.friend._id])
-                }
+      {currentUser.friends && currentUser.friends.length > 0 ? (
+        currentUser.friends
+          .filter(
+            (friendsItemPending: any) => friendsItemPending.pending == false
+          )
+          .map((friendsItem: any) => {
+            return (
+              <View
+                key={uuid.v4() as string}
+                style={{
+                  flex: 1,
+                  height: 49,
+                }}
               >
-                <FriendBar
-                  friendProfilePicture={friendsItem.friend.image}
-                  friendName={friendsItem.friend.firstName}
-                  friendSelected={
-                    shareWithFriendList.includes(friendsItem.friend._id)
-                      ? true
-                      : false
+                <TouchableOpacity
+                  onPress={() =>
+                    shareWithFriendList?.includes(friendsItem.friend._id)
+                      ? setShareWithFriendList(() =>
+                          shareWithFriendList.filter(
+                            (item) => item !== friendsItem.friend._id
+                          )
+                        )
+                      : setShareWithFriendList(() => [friendsItem.friend._id])
                   }
-                  pending={friendsItem.pending}
-                />
-              </TouchableOpacity>
-            </View>
-          );
-        })}
+                >
+                  <FriendBar
+                    friendProfilePicture={friendsItem.friend.image}
+                    friendName={friendsItem.friend.firstName}
+                    friendSelected={
+                      shareWithFriendList?.includes(friendsItem.friend._id)
+                        ? true
+                        : false
+                    }
+                    pending={friendsItem.pending}
+                  />
+                </TouchableOpacity>
+              </View>
+            );
+          })
+      ) : (
+        <></>
+      )}
     </ScrollView>
   );
 };
