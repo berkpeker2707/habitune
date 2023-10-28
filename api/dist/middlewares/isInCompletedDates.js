5d47ad50e38a6c3c8a8514a2294c621e8f4c0c41
Berk <berkpeker94@gmail.com>
2023-10-28 21:37:39 +0300

    new build for api

--- api/dist/middlewares/isInCompletedDates.js
+++ api/dist/middlewares/isInCompletedDates.js
@@ -2,9 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const isInCompletedDates = (array, value) => {
     return array.some((item) => {
-        var elemHave = new Date(new Date(item));
-        var elemToday = new Date(value);
-        const msBetweenDates = Math.abs(elemHave.getTime() - elemToday.getTime());
+        var alreadyStoredDate = new Date(new Date(item));
+        var dateTobeChecked = new Date(value);
+        const msBetweenDates = Math.abs(alreadyStoredDate.getTime() - dateTobeChecked.getTime());
         //convert ms to hours(min sec ms)
         const hoursBetweenDates = msBetweenDates / (60 * 60 * 1000);
         if (hoursBetweenDates < 24) {
