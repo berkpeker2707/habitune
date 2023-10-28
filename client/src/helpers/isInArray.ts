294bbc8cfce9b1eb5bbc7b9ea14867df3134a67d
Berk <berkpeker94@gmail.com>
2023-10-28 21:35:01 +0300

    added for loop to isInArray for perf

--- client/src/helpers/isInArray.ts
+++ client/src/helpers/isInArray.ts
@@ -1,32 +1,58 @@
-// import convertUTCDateToLocalDate from "./convertUTCDateToLocalDate";
+// // import convertUTCDateToLocalDate from "./convertUTCDateToLocalDate";
+
+// const isInArray = (array: any[], value: any) => {
+//   return array.some((item) => {
+//     // var alreadyStoredDate = new Date(convertUTCDateToLocalDate(new Date(item)));
+//     var alreadyStoredDate = new Date(new Date(item));
+
+//     // var dateTobeChecked = new Date(convertUTCDateToLocalDate(value));
+//     var dateTobeChecked = new Date(value);
+
+//     const msBetweenDates = Math.abs(
+//       alreadyStoredDate.getTime() - dateTobeChecked.getTime()
+//     );
+
+//     //convert ms to hours(min sec ms)
+//     const hoursBetweenDates = msBetweenDates / (60 * 60 * 1000);
+
+//     if (
+//       hoursBetweenDates < 24 &&
+//       alreadyStoredDate.getDate() === dateTobeChecked.getDate() &&
+//       alreadyStoredDate.getMonth() === dateTobeChecked.getMonth()
+//     ) {
+//       // console.log("date is within 24 hours");
+//       return true;
+//     } else {
+//       // console.log("date is NOT within 24 hours");
+//       return false;
+//     }
+//   });
+// };
+
+// export default isInArray;
 
 const isInArray = (array: any[], value: any) => {
-  return array.some((item) => {
-    // var alreadyStoredDate = new Date(convertUTCDateToLocalDate(new Date(item)));
-    var alreadyStoredDate = new Date(new Date(item));
+  const dateToBeChecked = new Date(value);
 
-    // var dateTobeChecked = new Date(convertUTCDateToLocalDate(value));
-    var dateTobeChecked = new Date(value);
+  for (const item of array) {
+    const alreadyStoredDate = new Date(item);
 
     const msBetweenDates = Math.abs(
-      alreadyStoredDate.getTime() - dateTobeChecked.getTime()
+      alreadyStoredDate.getTime() - dateToBeChecked.getTime()
     );
 
-    //convert ms to hours(min sec ms)
     const hoursBetweenDates = msBetweenDates / (60 * 60 * 1000);
 
     if (
       hoursBetweenDates < 24 &&
-      alreadyStoredDate.getDate() === dateTobeChecked.getDate() &&
-      alreadyStoredDate.getMonth() === dateTobeChecked.getMonth()
+      alreadyStoredDate.getDate() === dateToBeChecked.getDate() &&
+      alreadyStoredDate.getMonth() === dateToBeChecked.getMonth()
     ) {
-      // console.log("date is within 24 hours");
-      return true;
-    } else {
-      // console.log("date is NOT within 24 hours");
-      return false;
+      return true; // Found a match, return immediately.
     }
-  });
+  }
+
+  return false; // No match found in the array.
 };
 
 export default isInArray;
