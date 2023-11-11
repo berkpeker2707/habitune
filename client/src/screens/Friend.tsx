8d7486250d375d14656bedda475a35fa6b45c303
Berk <berkpeker94@gmail.com>
2023-11-11 23:30:13 +0300

    merged Friend with FriendInner

--- client/src/screens/Friend.tsx
+++ client/src/screens/Friend.tsx
@@ -1,8 +1,12 @@
 import * as React from "react";
 
-import FriendInner from "../components/friend/FriendInner";
+import { ScrollView, TextInput, View, Text } from "react-native";
+import DotGraph from "../components/overview/DotGraph";
+import StreakGraph from "../components/overview/StreakGraph";
+import SkeletonPlaceholder from "../components/skeleton/SkeletonPlaceholder";
+import { memo } from "react";
 
-const Friend = (props: any) => {
+const Friend = memo((props: any) => {
   const {
     dispatch,
     fetchAllHabitsAction,
@@ -16,22 +20,130 @@ const Friend = (props: any) => {
     friendCurrentHabitWeekStreakState,
     friendAllHabitDatesDotsState,
   } = props;
-
-  return (
-    <FriendInner
-      dispatch={dispatch}
-      fetchAllHabitsAction={fetchAllHabitsAction}
-      fetchAllHabitsOfSelectedUserAction={fetchAllHabitsOfSelectedUserAction}
-      allHabitsOfSelectedUser={allHabitsOfSelectedUser}
-      allHabitsOfSelectedUserNumber={allHabitsOfSelectedUserNumber}
-      habitLoading={habitLoading}
-      refreshing={refreshing}
-      setRefreshing={setRefreshing}
-      isItCurrentUser={isItCurrentUser}
-      friendCurrentHabitWeekStreakState={friendCurrentHabitWeekStreakState}
-      friendAllHabitDatesDotsState={friendAllHabitDatesDotsState}
-    />
-  );
-};
+  if (
+    (habitLoading && allHabitsOfSelectedUserNumber === 0) ||
+    (habitLoading && allHabitsOfSelectedUserNumber === undefined) ||
+    friendCurrentHabitWeekStreakState === undefined ||
+    friendAllHabitDatesDotsState === undefined ||
+    allHabitsOfSelectedUser === undefined
+  ) {
+    console.log(friendCurrentHabitWeekStreakState.length);
+    return (
+      <View
+        style={{
+          display: "flex",
+          height: "100%",
+          backgroundColor: "#FFFFFF",
+          justifyContent: "flex-start",
+          alignItems: "center",
+        }}
+      >
+        <ScrollView
+          style={{
+            marginBottom: 0,
+          }}
+        >
+          <TextInput
+            style={{
+              height: 29.5,
+              paddingLeft: 20,
+              color: "#444",
+              textAlign: "center",
+            }}
+            editable={false}
+            selectTextOnFocus={false}
+          >
+            Streaks Empty 😔
+          </TextInput>
+        </ScrollView>
+      </View>
+    );
+  } else if (
+    habitLoading &&
+    allHabitsOfSelectedUserNumber > 0 &&
+    friendCurrentHabitWeekStreakState.length > 0
+  ) {
+    return (
+      <View
+        style={{
+          display: "flex",
+          height: "100%",
+          backgroundColor: "#FFFFFF",
+          justifyContent: "flex-start",
+          alignItems: "center",
+        }}
+      >
+        <ScrollView
+          style={{
+            marginBottom: 0,
+          }}
+        >
+          <SkeletonPlaceholder
+            colorMode={"light"}
+            width={345}
+            height={39.5}
+            radius={0}
+          />
+        </ScrollView>
+      </View>
+    );
+  } else if (
+    !habitLoading &&
+    allHabitsOfSelectedUserNumber > 0 &&
+    friendCurrentHabitWeekStreakState.length > 0
+  ) {
+    return (
+      <View
+        style={{
+          display: "flex",
+          height: "100%",
+          backgroundColor: "#FFFFFF",
+          justifyContent: "flex-start",
+          alignItems: "center",
+        }}
+      >
+        <ScrollView
+          style={{
+            marginBottom: 85,
+          }}
+        >
+          <StreakGraph
+            allHabits={allHabitsOfSelectedUser}
+            currentHabitWeekStreak={friendCurrentHabitWeekStreakState}
+          />
+          <View style={{ margin: 20 }}></View>
+          <DotGraph
+            dispatch={dispatch}
+            fetchAllHabitsAction={fetchAllHabitsAction}
+            fetchAllHabitsOfSelectedUserAction={
+              fetchAllHabitsOfSelectedUserAction
+            }
+            allHabits={allHabitsOfSelectedUser}
+            allHabitsNumber={allHabitsOfSelectedUserNumber}
+            habitLoading={habitLoading}
+            refreshing={refreshing}
+            setRefreshing={setRefreshing}
+            isItCurrentUser={isItCurrentUser}
+            allHabitDatesDots={friendAllHabitDatesDotsState}
+          />
+        </ScrollView>
+      </View>
+    );
+  } else {
+    return (
+      <View
+        style={{
+          display: "flex",
+          height: "100%",
+          backgroundColor: "#FFFFFF",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text>"I have no memory of this place..." 🧙🏻</Text>
+      </View>
+    );
+  }
+});
 
 export default Friend;
