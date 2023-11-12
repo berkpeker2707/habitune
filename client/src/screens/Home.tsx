bdedc79779999ae9f5db4553fbb9f6b17e3c4b75
Berk <berkpeker94@gmail.com>
2023-11-12 17:20:43 +0300

    fixed Home and Overview prop types

--- client/src/screens/Home.tsx
+++ client/src/screens/Home.tsx
@@ -15,268 +15,300 @@ import SkeletonPlaceholder from "../components/skeleton/SkeletonPlaceholder";
 import ShareOpened from "../components/add/shareComponents/ShareOpened";
 import HabitBarParent from "../components/home/HabitBarParent";
 
-const Home = memo((props: any) => {
-  const {
-    dispatch,
-    fetchAllTodayHabitsAction,
-    updateHabitCompletedDateAction,
-    updateHabitSharedWithAction,
-    notificationSendAction,
-    currentUser,
-    allHabits,
-    allHabitsNumber,
-    currentHabitDatesIncluded,
-    homeEditBool,
-    setHomeEditBool,
-    habitLoading,
-    tempBarFilled,
-    setTempBarFilled,
-    refreshing,
-    setRefreshing,
-    shareWithFriendList,
-    setShareWithFriendList,
-    selectedItem,
-    setSelectedItem,
-    modalVisible,
-    setModalVisible,
-    setEditHabitSelected,
-    setHabitNameState,
-  } = props;
+const Home = memo(
+  (props: {
+    dispatch: Function;
+    fetchAllTodayHabitsAction: Function;
+    updateHabitCompletedDateAction: Function;
+    updateHabitSharedWithAction: Function;
+    notificationSendAction: Function;
+    currentUser: { firstName: string; friends: object[]; pending: boolean };
+    allHabits: [];
+    allHabitsNumber: number;
+    currentHabitDatesIncluded: [];
+    homeEditBool: boolean;
+    setHomeEditBool: Function;
+    habitLoading: boolean;
+    tempBarFilled: [boolean];
+    setTempBarFilled: Function;
+    refreshing: boolean;
+    setRefreshing: Function;
+    shareWithFriendList: [any];
+    setShareWithFriendList: Function;
+    selectedItem: string;
+    setSelectedItem: Function;
+    modalVisible: boolean;
+    setModalVisible: Function;
+    setEditHabitSelected: Function;
+    setHabitNameState: Function;
+  }) => {
+    const {
+      dispatch,
+      fetchAllTodayHabitsAction,
+      updateHabitCompletedDateAction,
+      updateHabitSharedWithAction,
+      notificationSendAction,
+      currentUser,
+      allHabits,
+      allHabitsNumber,
+      currentHabitDatesIncluded,
+      homeEditBool,
+      setHomeEditBool,
+      habitLoading,
+      tempBarFilled,
+      setTempBarFilled,
+      refreshing,
+      setRefreshing,
+      shareWithFriendList,
+      setShareWithFriendList,
+      selectedItem,
+      setSelectedItem,
+      modalVisible,
+      setModalVisible,
+      setEditHabitSelected,
+      setHabitNameState,
+    } = props;
 
-  const handleHabitClicked = useMemo(() => {
-    return (index: number) => {
-      const newHabitArray = tempBarFilled?.map((nH: any, i: number) => {
-        if (i === index) {
-          return !nH;
-        } else {
-          return nH;
-        }
-      });
-      setTempBarFilled(newHabitArray);
-    };
-  }, [tempBarFilled, setTempBarFilled]);
+    const handleHabitClicked = useMemo(() => {
+      return (index: number) => {
+        const newHabitArray = tempBarFilled?.map((nH: any, i: number) => {
+          if (i === index) {
+            return !nH;
+          } else {
+            return nH;
+          }
+        });
+        setTempBarFilled(newHabitArray);
+      };
+    }, [tempBarFilled, setTempBarFilled]);
 
-  useEffect(() => {
-    if (currentHabitDatesIncluded) {
+    useEffect(() => {
+      if (currentHabitDatesIncluded) {
+        setTempBarFilled(() => [...currentHabitDatesIncluded]);
+      }
+    }, [currentHabitDatesIncluded, refreshing]);
+
+    // useEffect(() => {
+    //   setHomeEditBool(false);
+    // }, []);
+
+    useEffect(() => {
+      if (homeEditBool === false) {
+        setSelectedItem(() => "");
+        setHabitNameState(() => "");
+      }
+    }, [homeEditBool]);
+
+    const onRefresh = useCallback(() => {
+      setRefreshing(true);
       setTempBarFilled(() => [...currentHabitDatesIncluded]);
-    }
-  }, [currentHabitDatesIncluded, refreshing]);
+      dispatch(
+        fetchAllTodayHabitsAction(
+          new Date(
+            new Date().getFullYear(),
+            new Date().getMonth(),
+            new Date().getDate(),
+            new Date().getHours(),
+            new Date().getMinutes(),
+            new Date().getSeconds()
+          ).getTime()
+        )
+      );
+      setTimeout(() => {
+        setRefreshing(false);
+      }, 2000);
+    }, []);
 
-  // useEffect(() => {
-  //   setHomeEditBool(false);
-  // }, []);
-
-  useEffect(() => {
-    if (homeEditBool === false) {
-      setSelectedItem(() => "");
-      setHabitNameState(() => "");
-    }
-  }, [homeEditBool]);
-
-  const onRefresh = useCallback(() => {
-    setRefreshing(true);
-    setTempBarFilled(() => [...currentHabitDatesIncluded]);
-    dispatch(
-      fetchAllTodayHabitsAction(
-        new Date(
-          new Date().getFullYear(),
-          new Date().getMonth(),
-          new Date().getDate(),
-          new Date().getHours(),
-          new Date().getMinutes(),
-          new Date().getSeconds()
-        ).getTime()
-      )
-    );
-    setTimeout(() => {
-      setRefreshing(false);
-    }, 2000);
-  }, []);
-
-  if (habitLoading) {
-    return (
-      <View
-        style={{
-          display: "flex",
-          height: "100%",
-          backgroundColor: "#FFFFFF",
-          justifyContent: "flex-start",
-          alignItems: "center",
-        }}
-      >
-        <Text>Loading...</Text>
-        <SkeletonPlaceholder
-          colorMode={"light"}
-          width={372}
-          height={48}
-          radius={20}
-        />
-      </View>
-    );
-  } else if (!habitLoading && allHabitsNumber === 0) {
-    return (
-      <View
-        style={{
-          display: "flex",
-          height: "100%",
-          backgroundColor: "#FFFFFF",
-          justifyContent: "flex-start",
-          alignItems: "center",
-        }}
-      >
-        <ScrollView
+    if (habitLoading) {
+      return (
+        <View
           style={{
-            marginBottom: 0,
+            display: "flex",
+            height: "100%",
+            backgroundColor: "#FFFFFF",
+            justifyContent: "flex-start",
+            alignItems: "center",
           }}
         >
-          <TextInput
+          <Text>Loading...</Text>
+          <SkeletonPlaceholder
+            colorMode={"light"}
+            width={372}
+            height={48}
+            radius={20}
+          />
+        </View>
+      );
+    } else if (!habitLoading && allHabitsNumber === 0) {
+      return (
+        <View
+          style={{
+            display: "flex",
+            height: "100%",
+            backgroundColor: "#FFFFFF",
+            justifyContent: "flex-start",
+            alignItems: "center",
+          }}
+        >
+          <ScrollView
             style={{
-              height: 29.5,
-              paddingLeft: 20,
-              color: "#444",
-              textAlign: "center",
+              marginBottom: 0,
             }}
-            editable={false}
-            selectTextOnFocus={false}
           >
-            Habits Empty 😔
-          </TextInput>
-        </ScrollView>
-      </View>
-    );
-  } else if (!habitLoading && allHabitsNumber > 0) {
-    return (
-      <>
-        <Modal
-          animationType="slide"
-          transparent={true}
-          visible={modalVisible}
-          onRequestClose={() => {
-            setModalVisible(!modalVisible);
-            setHomeEditBool(false);
-          }}
-        >
-          <View
-            style={{
-              flex: 1,
-              justifyContent: "center",
-              alignItems: "center",
-              marginTop: 22,
-              backgroundColor: "rgba(52, 52, 52, 0.8)",
+            <TextInput
+              style={{
+                height: 29.5,
+                paddingLeft: 20,
+                color: "#444",
+                textAlign: "center",
+              }}
+              editable={false}
+              selectTextOnFocus={false}
+            >
+              Habits Empty 😔
+            </TextInput>
+          </ScrollView>
+        </View>
+      );
+    } else if (!habitLoading && allHabitsNumber > 0) {
+      return (
+        <>
+          <Modal
+            animationType="slide"
+            transparent={true}
+            visible={modalVisible}
+            onRequestClose={() => {
+              setModalVisible(!modalVisible);
+              setHomeEditBool(false);
             }}
           >
             <View
               style={{
-                margin: 20,
-                backgroundColor: "white",
-                borderRadius: 20,
-                padding: 35,
+                flex: 1,
+                justifyContent: "center",
                 alignItems: "center",
-                shadowColor: "#000",
-                shadowOffset: {
-                  width: 0,
-                  height: 2,
-                },
-                shadowOpacity: 0.25,
-                shadowRadius: 4,
-                elevation: 5,
+                marginTop: 22,
+                backgroundColor: "rgba(52, 52, 52, 0.8)",
               }}
             >
-              <ShareOpened
-                currentUser={currentUser}
-                shareWithFriendList={shareWithFriendList}
-                setShareWithFriendList={setShareWithFriendList}
-              />
-
-              <Pressable
-                style={[
-                  { borderRadius: 20, padding: 10, elevation: 2 },
-                  { backgroundColor: "#968EB0" },
-                ]}
-                onPressIn={() => Vibration.vibrate(10)}
-                onPress={() => {
-                  setModalVisible(!modalVisible);
-                  dispatch(
-                    updateHabitSharedWithAction({
-                      _id: selectedItem,
-                      userId: shareWithFriendList[0],
-                    })
-                  );
-                  setHomeEditBool(false);
+              <View
+                style={{
+                  margin: 20,
+                  backgroundColor: "white",
+                  borderRadius: 20,
+                  padding: 35,
+                  alignItems: "center",
+                  shadowColor: "#000",
+                  shadowOffset: {
+                    width: 0,
+                    height: 2,
+                  },
+                  shadowOpacity: 0.25,
+                  shadowRadius: 4,
+                  elevation: 5,
                 }}
               >
-                <Text
-                  style={{
-                    color: "white",
-                    fontWeight: "bold",
-                    textAlign: "center",
+                <ShareOpened
+                  currentUser={currentUser}
+                  shareWithFriendList={shareWithFriendList}
+                  setShareWithFriendList={setShareWithFriendList}
+                />
+
+                <Pressable
+                  style={[
+                    { borderRadius: 20, padding: 10, elevation: 2 },
+                    { backgroundColor: "#968EB0" },
+                  ]}
+                  onPressIn={() => Vibration.vibrate(10)}
+                  onPress={() => {
+                    setModalVisible(!modalVisible);
+                    dispatch(
+                      updateHabitSharedWithAction({
+                        _id: selectedItem,
+                        userId: shareWithFriendList[0],
+                      })
+                    );
+                    setHomeEditBool(false);
                   }}
                 >
-                  Share
-                </Text>
-              </Pressable>
+                  <Text
+                    style={{
+                      color: "white",
+                      fontWeight: "bold",
+                      textAlign: "center",
+                    }}
+                  >
+                    Share
+                  </Text>
+                </Pressable>
+              </View>
             </View>
-          </View>
-        </Modal>
-        <TouchableWithoutFeedback
-          onBlur={() => {
-            if (!modalVisible) {
-              setHomeEditBool(false);
-            }
-          }}
-        >
-          <View
-            style={{
-              display: "flex",
-              height: "100%",
-              backgroundColor: "#FFFFFF",
-              justifyContent: "flex-start",
-              alignItems: "center",
+          </Modal>
+          <TouchableWithoutFeedback
+            onBlur={() => {
+              if (!modalVisible) {
+                setHomeEditBool(false);
+              }
             }}
           >
-            <ScrollView
+            <View
               style={{
-                marginBottom: 85,
+                display: "flex",
+                height: "100%",
+                backgroundColor: "#FFFFFF",
+                justifyContent: "flex-start",
+                alignItems: "center",
               }}
-              refreshControl={
-                <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
-              }
             >
-              <Text>Habits</Text>
-              <HabitBarParent
-                dispatch={dispatch}
-                updateHabitCompletedDateAction={updateHabitCompletedDateAction}
-                notificationSendAction={notificationSendAction}
-                currentUser={currentUser}
-                allHabits={allHabits}
-                tempBarFilled={tempBarFilled}
-                setHomeEditBool={setHomeEditBool}
-                selectedItem={selectedItem}
-                setSelectedItem={setSelectedItem}
-                handleHabitClicked={handleHabitClicked}
-                setEditHabitSelected={setEditHabitSelected}
-                setHabitNameState={setHabitNameState}
-              />
-            </ScrollView>
-          </View>
-        </TouchableWithoutFeedback>
-      </>
-    );
-  } else {
-    return (
-      <View
-        style={{
-          display: "flex",
-          height: "100%",
-          backgroundColor: "#FFFFFF",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Text>"I have no memory of this place..." 🧙🏻</Text>
-      </View>
-    );
+              <ScrollView
+                style={{
+                  marginBottom: 85,
+                }}
+                refreshControl={
+                  <RefreshControl
+                    refreshing={refreshing}
+                    onRefresh={onRefresh}
+                  />
+                }
+              >
+                <Text>Habits</Text>
+                <HabitBarParent
+                  dispatch={dispatch}
+                  updateHabitCompletedDateAction={
+                    updateHabitCompletedDateAction
+                  }
+                  notificationSendAction={notificationSendAction}
+                  currentUser={currentUser}
+                  allHabits={allHabits}
+                  tempBarFilled={tempBarFilled}
+                  setHomeEditBool={setHomeEditBool}
+                  selectedItem={selectedItem}
+                  setSelectedItem={setSelectedItem}
+                  handleHabitClicked={handleHabitClicked}
+                  setEditHabitSelected={setEditHabitSelected}
+                  setHabitNameState={setHabitNameState}
+                />
+              </ScrollView>
+            </View>
+          </TouchableWithoutFeedback>
+        </>
+      );
+    } else {
+      return (
+        <View
+          style={{
+            display: "flex",
+            height: "100%",
+            backgroundColor: "#FFFFFF",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Text>"I have no memory of this place..." 🧙🏻</Text>
+        </View>
+      );
+    }
   }
-});
+);
 
 export default Home;
--- client/src/screens/Overview.tsx
+++ client/src/screens/Overview.tsx
@@ -6,137 +6,153 @@ import DotGraph from "../components/overview/DotGraph";
 import StreakGraph from "../components/overview/StreakGraph";
 import SkeletonPlaceholder from "../components/skeleton/SkeletonPlaceholder";
 
-const Overview = memo((props: any) => {
-  const {
-    dispatch,
-    fetchAllHabitsAction,
-    fetchAllHabitsOfSelectedUserAction,
-    allHabits,
-    allHabitsNumber,
-    habitLoading,
-    refreshing,
-    setRefreshing,
-    isItCurrentUser,
-    currentHabitWeekStreakState,
-    allHabitDatesDots,
-  } = props;
+const Overview = memo(
+  (props: {
+    dispatch: Function;
+    fetchAllHabitsAction: Function;
+    fetchAllHabitsOfSelectedUserAction: Function;
+    allHabits: [];
+    allHabitsNumber: number;
+    habitLoading: boolean;
+    refreshing: boolean;
+    setRefreshing: Function;
+    isItCurrentUser: boolean;
+    currentHabitWeekStreakState: [];
+    allHabitDatesDots: [];
+  }) => {
+    const {
+      dispatch,
+      fetchAllHabitsAction,
+      fetchAllHabitsOfSelectedUserAction,
+      allHabits,
+      allHabitsNumber,
+      habitLoading,
+      refreshing,
+      setRefreshing,
+      isItCurrentUser,
+      currentHabitWeekStreakState,
+      allHabitDatesDots,
+    } = props;
 
-  if (habitLoading) {
-    return (
-      <View
-        style={{
-          display: "flex",
-          height: "100%",
-          backgroundColor: "#FFFFFF",
-          justifyContent: "flex-start",
-          alignItems: "center",
-        }}
-      >
-        <ScrollView
+    if (habitLoading) {
+      return (
+        <View
           style={{
-            marginBottom: 0,
+            display: "flex",
+            height: "100%",
+            backgroundColor: "#FFFFFF",
+            justifyContent: "flex-start",
+            alignItems: "center",
           }}
         >
-          <Text>Loading...</Text>
-          <SkeletonPlaceholder
-            colorMode={"light"}
-            width={345}
-            height={39.5}
-            radius={0}
-          />
-        </ScrollView>
-      </View>
-    );
-  } else if (!habitLoading && allHabitsNumber === 0) {
-    return (
-      <View
-        style={{
-          display: "flex",
-          height: "100%",
-          backgroundColor: "#FFFFFF",
-          justifyContent: "flex-start",
-          alignItems: "center",
-        }}
-      >
-        <ScrollView
-          style={{
-            marginBottom: 0,
-          }}
-        >
-          <TextInput
+          <ScrollView
             style={{
-              height: 29.5,
-              paddingLeft: 20,
-              color: "#444",
-              textAlign: "center",
+              marginBottom: 0,
             }}
-            editable={false}
-            selectTextOnFocus={false}
           >
-            Streaks Empty 😔
-          </TextInput>
-        </ScrollView>
-      </View>
-    );
-  } else if (!habitLoading && allHabitsNumber > 0) {
-    return (
-      <View
-        style={{
-          display: "flex",
-          height: "100%",
-          backgroundColor: "#FFFFFF",
-          justifyContent: "flex-start",
-          alignItems: "center",
-        }}
-      >
-        <ScrollView
+            <Text>Loading...</Text>
+            <SkeletonPlaceholder
+              colorMode={"light"}
+              width={345}
+              height={39.5}
+              radius={0}
+            />
+          </ScrollView>
+        </View>
+      );
+    } else if (!habitLoading && allHabitsNumber === 0) {
+      return (
+        <View
           style={{
-            marginBottom: 85,
+            display: "flex",
+            height: "100%",
+            backgroundColor: "#FFFFFF",
+            justifyContent: "flex-start",
+            alignItems: "center",
           }}
         >
-          {currentHabitWeekStreakState.some((value: number) => value !== 0) ? (
-            <>
-              <StreakGraph
-                allHabits={allHabits}
-                currentHabitWeekStreak={currentHabitWeekStreakState}
-              />
-            </>
-          ) : (
-            <></>
-          )}
-          <View style={{ margin: 20 }}></View>
-          <DotGraph
-            dispatch={dispatch}
-            fetchAllHabitsAction={fetchAllHabitsAction}
-            fetchAllHabitsOfSelectedUserAction={
-              fetchAllHabitsOfSelectedUserAction
-            }
-            allHabits={allHabits}
-            allHabitsNumber={allHabitsNumber}
-            habitLoading={habitLoading}
-            refreshing={refreshing}
-            setRefreshing={setRefreshing}
-            isItCurrentUser={isItCurrentUser}
-            allHabitDatesDots={allHabitDatesDots}
-          />
-        </ScrollView>
-      </View>
-    );
-  } else {
-    return (
-      <View
-        style={{
-          display: "flex",
-          height: "100%",
-          backgroundColor: "#FFFFFF",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Text>"I have no memory of this place..." 🧙🏻</Text>
-      </View>
-    );
+          <ScrollView
+            style={{
+              marginBottom: 0,
+            }}
+          >
+            <TextInput
+              style={{
+                height: 29.5,
+                paddingLeft: 20,
+                color: "#444",
+                textAlign: "center",
+              }}
+              editable={false}
+              selectTextOnFocus={false}
+            >
+              Streaks Empty 😔
+            </TextInput>
+          </ScrollView>
+        </View>
+      );
+    } else if (!habitLoading && allHabitsNumber > 0) {
+      return (
+        <View
+          style={{
+            display: "flex",
+            height: "100%",
+            backgroundColor: "#FFFFFF",
+            justifyContent: "flex-start",
+            alignItems: "center",
+          }}
+        >
+          <ScrollView
+            style={{
+              marginBottom: 85,
+            }}
+          >
+            {currentHabitWeekStreakState.some(
+              (value: number) => value !== 0
+            ) ? (
+              <>
+                <StreakGraph
+                  allHabits={allHabits}
+                  currentHabitWeekStreak={currentHabitWeekStreakState}
+                />
+              </>
+            ) : (
+              <></>
+            )}
+            <View style={{ margin: 20 }}></View>
+            <DotGraph
+              dispatch={dispatch}
+              fetchAllHabitsAction={fetchAllHabitsAction}
+              fetchAllHabitsOfSelectedUserAction={
+                fetchAllHabitsOfSelectedUserAction
+              }
+              allHabits={allHabits}
+              allHabitsNumber={allHabitsNumber}
+              habitLoading={habitLoading}
+              refreshing={refreshing}
+              setRefreshing={setRefreshing}
+              isItCurrentUser={isItCurrentUser}
+              allHabitDatesDots={allHabitDatesDots}
+            />
+          </ScrollView>
+        </View>
+      );
+    } else {
+      return (
+        <View
+          style={{
+            display: "flex",
+            height: "100%",
+            backgroundColor: "#FFFFFF",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Text>"I have no memory of this place..." 🧙🏻</Text>
+        </View>
+      );
+    }
   }
-});
+);
 
 export default Overview;
