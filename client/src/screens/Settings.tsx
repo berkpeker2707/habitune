141f5b5bbb0b52883b2630339b08186759cf8bfe
Berk <berkpeker94@gmail.com>
2023-11-29 20:57:42 +0300

    settings version change

--- client/src/screens/Settings.tsx
+++ client/src/screens/Settings.tsx
@@ -277,7 +277,7 @@ const Settings = (props: {
           marginBottom: 105,
         }}
       >
-        <Text style={{ color: theme.fadedPrimaryText }}>v1.2.2</Text>
+        <Text style={{ color: theme.fadedPrimaryText }}>v1.3.0</Text>
       </View>
     </View>
   );
