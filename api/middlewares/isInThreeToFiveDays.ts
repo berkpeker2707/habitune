a2d1c3159f11ffbb893520afad491f38ec72618b
Berk <berkpeker94@gmail.com>
2023-12-03 15:56:07 +0300

    reduced all to 6 hours daily and changed isInThreeToFiveDays to check between 3 to 7

--- api/middlewares/isInThreeToFiveDays.ts
+++ api/middlewares/isInThreeToFiveDays.ts
@@ -10,7 +10,7 @@ const isInThreeToFiveDays = (array: any[], value: Date) => {
     //convert ms to days
     const daysBetweenDates = msBetweenDates / (24 * 60 * 60 * 1000);
 
-    if (daysBetweenDates >= 3 && daysBetweenDates < 5) {
+    if (daysBetweenDates >= 3 && daysBetweenDates < 7) {
       return true;
     } else {
       return false;
