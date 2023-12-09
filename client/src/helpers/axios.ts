4333db4640e7d3aef258538474493ef6cc35e0ad
Berk <berkpeker94@gmail.com>
2023-12-10 00:37:01 +0300

    network thingy

--- client/src/helpers/axios.ts
+++ client/src/helpers/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  // baseURL: "http://192.168.1.33:1111/api",
+  // baseURL: "http://192.168.1.37:1111/api",
   baseURL: "https://www.habitune.net/api",
 });
 
