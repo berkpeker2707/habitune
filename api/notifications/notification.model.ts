f441ffb18d3a983aca43f96a93d8bb972ad90f3c
Berk <berkpeker94@gmail.com>
2023-12-10 18:34:31 +0300

    fixed notification api for unique notification doc with timer

--- api/notifications/notification.model.ts
+++ api/notifications/notification.model.ts
@@ -4,18 +4,45 @@ const notificationSchema = new Schema<any>({
   userID: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "User",
-    unique: true,
-    index: true,
+    // unique: true,
+    // index: true,
   },
-  tokenID: {
+  habitID: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Habit",
+    // unique: true,
+    // index: true,
+  },
+  notificationTitle: {
     type: String,
   },
-  notifications: {
-    type: [Object],
+  notificationBody: {
+    type: String,
+  },
+  notificationImageUrl: {
+    type: String,
+  },
+  notificationFriend: {
+    type: Array,
+  },
+  notificationFriendImage: {
+    type: Array,
+  },
+  notificationFirstName: {
+    type: String,
+  },
+  notificationHabitName: {
+    type: String,
   },
   createdAt: {
     type: Date,
     default: Date.now,
+  },
+  expirationDate: {
+    type: Date,
+    // default: () => new Date(Date.now() + 1 * 60 * 1000), //1 minutes in milliseconds
+    default: () => new Date(Date.now() + 15 * 24 * 60 * 60 * 1000), // 15 days in milliseconds
+    index: { expires: "5m" }, //create a TTL (Time-To-Live) index, where documents expire after 5 minute
   },
 });
 
