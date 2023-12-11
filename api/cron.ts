537b04602bae33a5a63bf1f0c3b70b1e9bba7bd4
Berk <berkpeker94@gmail.com>
2023-12-11 21:55:45 +0300

    new cron

--- api/cron.ts
+++ api/cron.ts
@@ -1,3 +1,6 @@
+import { notifyUser } from "./notifications/notification.reminders";
+
 export default function handler(req: any, res: any) {
+  notifyUser();
   res.status(200).end("Hello Cron!");
 }
