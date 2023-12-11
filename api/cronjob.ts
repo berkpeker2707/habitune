6cba774ff3912b14ef0da8288570832a546648dc
Berk <berkpeker94@gmail.com>
2023-12-11 23:20:35 +0300

    cronjob log fix

--- api/cronjob.ts
+++ api/cronjob.ts
@@ -1,13 +1,12 @@
-import Logger from "./middlewares/logger";
 import { notifyUser } from "./notifications/notification.reminders";
 
 export default async (req: any, res: any) => {
   try {
     await notifyUser();
-    Logger.info("Cron job executed successfully");
+    console.log("Cron job executed successfully");
     res.status(200).send("Cron job executed successfully");
   } catch (error) {
-    Logger.info("Error executing cron job");
+    console.log("Error executing cron job");
     res.status(500).send("Internal Server Error");
   }
 };
