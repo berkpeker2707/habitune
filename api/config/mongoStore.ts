7c5e78f50d2cd8ec3923bfd4339a8f4fcdcb8ebc
Berk <berkpeker94@gmail.com>
2023-12-14 18:57:52 +0300

    fix mongodb store try catch

--- api/config/mongoStore.ts
+++ api/config/mongoStore.ts
@@ -3,18 +3,20 @@ const MongoDBStore = require("connect-mongodb-session")(session);
 require("dotenv").config();
 
 const mongoStore = () => {
-  const store = new MongoDBStore({
-    uri: process.env.MONGODB_URI,
-    collection: "sessions", //collection to store sessions in
-    expires: 1000 * 60 * 60 * 24 * 7, //session expiration time (7 days)
-    autoRemove: "native", //automatically remove expired sessions
-  });
-
-  store.on("error", function (error: any) {
-    console.log(`MongoDBStore Error: ${error}`);
-  });
-
-  return store;
+  try {
+    const store = new MongoDBStore({
+      uri: process.env.MONGODB_URI,
+      collection: "sessions", //collection to store sessions in
+      expires: 1000 * 60 * 60 * 24 * 7, //session expiration time (7 days)
+      autoRemove: "native", //automatically remove expired sessions
+    });
+    return store;
+  } catch (error) {
+    console.log(error);
+  }
+  // store.on("error", function (error: any) {
+  //   console.log(`MongoDBStore Error: ${error}`);
+  // });
 };
 
 export default mongoStore;
