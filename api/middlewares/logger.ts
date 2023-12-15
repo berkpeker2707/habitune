c5c2fec44f13b8c2137b66018403a9296d5f6e41
Berk <berkpeker94@gmail.com>
2023-12-15 19:35:12 +0300

    fixed logger.ts and seperated httpLogger, infoLogger, errorLogger, warnLogger and added dev and production env options

--- api/middlewares/logger.ts
+++ api/middlewares/logger.ts
@@ -1,6 +1,7 @@
 import winston from "winston";
 import winston_db from "winston-mongodb";
-// import path from "path";
+import path from "path";
+const env = process.env.NODE_ENV || "development";
 
 const mongodbConnectionString = process.env.MONGODB_LOG_URI;
 
@@ -8,91 +9,99 @@ if (!mongodbConnectionString) {
   throw new Error("MongoDB connection string is not provided.");
 }
 
-const levels = {
-  error: 0,
-  warn: 1,
-  info: 2,
-  http: 3,
-  debug: 4,
-};
-
-const level = () => {
-  const env = process.env.NODE_ENV || "development";
-  const isDevelopment = env === "development";
-  return isDevelopment ? "debug" : "warn";
-};
-
-const colors = {
-  error: "red",
-  warn: "yellow",
-  info: "green",
-  http: "magenta",
-  debug: "white",
-};
-
-winston.addColors(colors);
-
 const format = winston.format.combine(
   winston.format.timestamp({ format: "YYYY-MM-DD HH:mm:ss:ms" }),
-  winston.format.colorize({ all: true }),
   winston.format.printf(
     (info) => `${info.timestamp} ${info.level}: ${info.message}`
   )
 );
 
-const transports = [
-  new winston.transports.Console(),
-
-  // new winston.transports.File({
-  //   filename: path.join(__dirname, "..", "logs/error.log"),
-  //   level: "error",
-  // }),
-  // new winston.transports.File({
-  //   filename: path.join(__dirname, "..", "logs/warn.log"),
-  //   level: "warn",
-  // }),
-  // new winston.transports.File({
-  //   filename: path.join(__dirname, "..", "logs/info.log"),
-  //   level: "info",
-  // }),
-  // new winston.transports.File({
-  //   filename: path.join(__dirname, "..", "logs/http.log"),
-  //   level: "http",
-  // }),
-
-  // new winston_db.MongoDB({
-  //   db: mongodbConnectionString,
-  //   options: { useNewUrlParser: true, useUnifiedTopology: true },
-  //   collection: "error_logs",
-  //   level: "error",
-  // }),
-
-  // new winston_db.MongoDB({
-  //   db: mongodbConnectionString,
-  //   options: { useNewUrlParser: true, useUnifiedTopology: true },
-  //   collection: "warn_logs",
-  //   level: "warn",
-  // }),
-  // new winston_db.MongoDB({
-  //   db: mongodbConnectionString,
-  //   options: { useNewUrlParser: true, useUnifiedTopology: true },
-  //   collection: "info_logs",
-  //   level: "info",
-  // }),
-  // new winston_db.MongoDB({
-  //   db: mongodbConnectionString,
-  //   options: { useNewUrlParser: true, useUnifiedTopology: true },
-  //   collection: "http_logs",
-  //   level: "http",
-  // }),
-];
-
-const Logger = winston.createLogger({
-  level: level(),
-  levels,
+const httpLogger = winston.createLogger({
   format,
-  defaultMeta: { service: "user-service" },
-  transports,
+  defaultMeta: { service: "http-service" },
+  transports:
+    env === "development"
+      ? [
+          new winston.transports.Console(),
+          new winston.transports.File({
+            filename: path.join(__dirname, "..", "logs/http.log"),
+            level: "http",
+          }),
+        ]
+      : [
+          new winston_db.MongoDB({
+            db: mongodbConnectionString,
+            options: { useNewUrlParser: true, useUnifiedTopology: true },
+            collection: "http_logs",
+            level: "http",
+          }),
+        ],
 });
 
-export default Logger;
+const infoLogger = winston.createLogger({
+  format,
+  defaultMeta: { service: "info-service" },
+  transports:
+    env === "development"
+      ? [
+          new winston.transports.Console(),
+          new winston.transports.File({
+            filename: path.join(__dirname, "..", "logs/info.log"),
+            level: "info",
+          }),
+        ]
+      : [
+          new winston_db.MongoDB({
+            db: mongodbConnectionString,
+            options: { useNewUrlParser: true, useUnifiedTopology: true },
+            collection: "info_logs",
+            level: "info",
+          }),
+        ],
+});
+
+const errorLogger = winston.createLogger({
+  format,
+  defaultMeta: { service: "error-service" },
+  transports:
+    env === "development"
+      ? [
+          new winston.transports.Console(),
+          new winston.transports.File({
+            filename: path.join(__dirname, "..", "logs/error.log"),
+            level: "error",
+          }),
+        ]
+      : [
+          new winston_db.MongoDB({
+            db: mongodbConnectionString,
+            options: { useNewUrlParser: true, useUnifiedTopology: true },
+            collection: "error_logs",
+            level: "error",
+          }),
+        ],
+});
+
+const warnLogger = winston.createLogger({
+  format,
+  defaultMeta: { service: "warn-service" },
+  transports:
+    env === "development"
+      ? [
+          new winston.transports.Console(),
+          new winston.transports.File({
+            filename: path.join(__dirname, "..", "logs/warn.log"),
+            level: "warn",
+          }),
+        ]
+      : [
+          new winston_db.MongoDB({
+            db: mongodbConnectionString,
+            options: { useNewUrlParser: true, useUnifiedTopology: true },
+            collection: "warn_logs",
+            level: "warn",
+          }),
+        ],
+});
+
+export { httpLogger, infoLogger, errorLogger, warnLogger };
