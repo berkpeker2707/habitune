4fa5ee5a206e55d4f7ad0bcd9cfdaa4e6e8c45c2
Berk <berkpeker94@gmail.com>
2023-12-28 14:33:52 +0300

    new build for mac

--- client/src/helpers/axios.ts
+++ client/src/helpers/axios.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  // baseURL: "http://192.168.1.38:1111/api",
-  baseURL: "https://www.habitune.net/api",
+  baseURL: "http://192.168.1.40:1111/api",
+  // baseURL: "https://www.habitune.net/api",
 });
 
 export default axiosInstance;
