2f09922b09b280c64b49361e7dc3c795552d8375
Berk <berkpeker94@gmail.com>
2024-01-06 21:40:11 +0300

    axios dev fix

--- client/src/helpers/axios.ts
+++ client/src/helpers/axios.ts
@@ -1,8 +1,13 @@
 import axios from "axios";
 
+const isDev = process.env.NODE_ENV === "development";
+
+const baseURL = isDev
+  ? "http://192.168.1.69:1111/api"
+  : "https://www.habitune.net/api";
+
 const axiosInstance = axios.create({
-  baseURL: "http://192.168.1.69:1111/api",
-  // baseURL: "https://www.habitune.net/api",
+  baseURL: baseURL,
 });
 
 export default axiosInstance;
