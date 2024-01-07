6f5f71c233b68bad136fd57efb5c225be1465614
Berk <berkpeker94@gmail.com>
2024-01-07 13:55:49 +0300

    added ignore large state warning to redux and network thingy

--- client/src/helpers/axios.ts
+++ client/src/helpers/axios.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const isDev = process.env.NODE_ENV === "development";
 
 const baseURL = isDev
-  ? "http://192.168.1.69:1111/api"
+  ? "http://192.168.1.34:1111/api"
   : "https://www.habitune.net/api";
 
 const axiosInstance = axios.create({
--- client/src/state/store.ts
+++ client/src/state/store.ts
@@ -40,6 +40,7 @@ export const store = configureStore({
       // serializableCheck: {
       //   ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
       // },
+      immutableCheck: false, //to ignore large state object warnings
       serializableCheck: false,
     }),
 });
