02f8c5f3db191d07a79bff4e25d433267051a571
NomFindarato <berkpeker94@gmail.com>
2024-01-08 17:14:11 +0300

    Update calculateUpcomingDates.ts

--- api/middlewares/calculateUpcomingDates.ts
+++ api/middlewares/calculateUpcomingDates.ts
@@ -44,8 +44,7 @@ const calculateUpcomingDates = async (
     if (re.test(tempDate.toDateString())) result.push(new Date(tempDate));
     tempDate.setDate(tempDate.getDate() + 1);
   }
-  //result.forEach((day) => day.getTime() + dayTimeInMilliseconds);
-  result.forEach((day) => day.setTime(day.getTime()));
+  result.forEach((day) => day.getTime() + dayTimeInMilliseconds);
 
   return result;
 };
